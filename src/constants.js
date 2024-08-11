a18876ab393f13a5fa8caacd7693bea6ae5c2d38
Rohan Sirsa <rohansirsa052@gmail.com>
2024-08-11 19:06:20 +0530

    second commit

---
+++ src/constants.js
@@ -0,0 +1,49 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Rohan';
+
+export const experiences = [
+    {
+        'company': 'IIT Kanpur',
+        'role': 'Web Development, Intern',
+        'duration': 'April 2023 - September 2023',
+        'logo': 'https://www.iitk.ac.in/new/images/page-images/logo/redlogo.jpg',
+        'points': [
+            'Actively contribute to the development of the IIT Kanpur Virtual Lab, a cutting-edge platform designed to provide students with remoteaccess to laboratory experiments via virtual simulations and online tools..',
+            'Collaborate with a cross-functional team of developers and researchers to create an engaging and user-friendly web interface for thevirtual lab',
+            'Design and develop interactive simulations of scientific instruments using JavaScript and HTML Canvas to enhance students understanding of complex scientific concepts📈.',
+        ],
+        'url': 'https://lab.iabhitech.com/',
+    },
+    {
+        'company': 'FinxSystms',
+        'role': 'Rest API Research Intern',
+        'duration': 'July 2023 - January 202',
+        'logo': 'https://media.licdn.com/dms/image/D560BAQExWSdql90vCw/company-logo_200_200/0/1695320196858/finxsassessment_logo?e=2147483647&v=beta&t=f9H679IkYb6PBJP6x5WBUa55-CdP_yciYgY-mkzKVJI',
+        'points': [
+            'Integrated additional tools for more information capture aiding in bug and issue resolution contributing to a significant improvement in Bug Analysis..',
+            'Developed a company website using the MERN stack, Tailwind CSS, and Bootstrap'
+        ],
+        'url': 'https://finxsystems.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
